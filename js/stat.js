1eab467ab1b407601e5dd9fa2babe15945272571
Rex4r <K.cnhf72>
2018-06-19 00:21:05 +0700

    module2-task1

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var renderStatistics = function (ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = '#ffffff';
@@ -22,7 +22,7 @@ var renderStatistics = function (ctx, names, times) {
     var histogramHeight = Math.round(times[i] * HISTOGRAMS_MAX_HEIGHT / maxTime);
     var histogramX = 150 + (HISTOGRAMS_WIDTH + HISTOGRAMS_MARGIN) * i;
     var histogramY = 90 + (HISTOGRAMS_MAX_HEIGHT - histogramHeight);
-    var histogramColor = 'rgba(0, 0, ' + (names[i] === 'Вы' ? 255 : Math.round(Math.random() * 255)) + ', 1)'
+    var histogramColor = 'rgba(0, 0, ' + (names[i] === 'Вы' ? 255 : Math.round(Math.random() * 255)) + ', 1)';
     ctx.fillStyle = histogramColor;
     ctx.fillRect(histogramX, histogramY, HISTOGRAMS_WIDTH, histogramHeight);
     ctx.fillText(names[i], histogramX, 270);
