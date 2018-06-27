9cd297e2521da6b4abe2b6dae487448ed524ef16
capenkod <tsapenko.danil@allsoft.ru>
2018-06-27 14:59:36 +0700

    review fix

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
+var createFieldOnCanvas = function(ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = '#ffffff';
@@ -9,32 +9,34 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 150, 40);
   ctx.fillText('Список результатов:', 150, 60);
-  var getMaxTime = function (playersTimes) {
-    var maxTime = playersTimes[0];
-    for (var i = 0; i < playersTimes.length; i++) {
-      if (maxTime < playersTimes[i]) {
-        maxTime = playersTimes[i];
-      }
+};
+var getMax = function (arr) {
+  var max = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
     }
-    return maxTime;
-  };
-  var maxTime = getMaxTime(times);
-  var HISTOGRAMS_WIDTH = 40;
-  var HISTOGRAMS_MAX_HEIGHT = 150;
-  var HISTOGRAMS_MARGIN = 50;
-  var createHistogram = function (width, margin, maxHeight, maxPlayerTime, playerTime, playerName, playerNumber) {
-    var histogramHeight = Math.round(playerTime * HISTOGRAMS_MAX_HEIGHT / maxPlayerTime);
-    var histogramX = 150 + (HISTOGRAMS_WIDTH + HISTOGRAMS_MARGIN) * playerNumber;
-    var histogramY = 90 + (HISTOGRAMS_MAX_HEIGHT - histogramHeight);
+  }
+  return max;
+};
+var createHistograms = function (ctx, width, margin, maxHeight, maxPlayerTime, playerTimes, playerNames) {
+  for (var i = 0; i < playerNames.length; i++) {
+    var histogramHeight = Math.round(playerTimes[i] * maxHeight / maxPlayerTime);
+    var histogramX = 150 + (width + margin) * i;
+    var histogramY = 90 + (maxHeight - histogramHeight);
     var histogramColor = 'rgba(0, 0, ' + Math.round(Math.random() * 255) + ', 1)';
-    if (playerName === 'Вы') {
+    if (playerNames[i] === 'Вы') {
       histogramColor = 'rgba(255, 0, 0, 1)';
     }
     ctx.fillStyle = histogramColor;
-    ctx.fillRect(histogramX, histogramY, HISTOGRAMS_WIDTH, histogramHeight);
-    ctx.fillText(playerName, histogramX, 270);
-  };
-  for (var i = 0; i < times.length; i++) {
-    createHistogram(HISTOGRAMS_WIDTH, HISTOGRAMS_MARGIN, HISTOGRAMS_MAX_HEIGHT, maxTime, times[i], names[i], i);
+    ctx.fillRect(histogramX, histogramY, width, histogramHeight);
+    ctx.fillText(playerNames[i], histogramX, 270);
   }
+};
+window.renderStatistics = function (ctx, names, times) {
+  var HISTOGRAMS_WIDTH = 40;
+  var HISTOGRAMS_MAX_HEIGHT = 150;
+  var HISTOGRAMS_MARGIN = 50;
+  createFieldOnCanvas(ctx);
+  createHistograms(ctx, HISTOGRAMS_WIDTH, HISTOGRAMS_MARGIN, HISTOGRAMS_MAX_HEIGHT, getMax(times), times, names);
 };
